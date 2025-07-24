f2403a2a0996e562c3cc044c62e6e31ba6096ea7
Shayaan <aysajjad@icloud.com>
2025-07-24 10:25:15 -0400

    auto-commit: 2025-07-24 10:25:15

--- components/command-palette.tsx
+++ components/command-palette.tsx
@@ -402,7 +402,7 @@ export default function CommandPalette({
                   <span>toggle theme</span>
                 </div>
                 <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-                  ⇧D
+                  shift + d
                 </kbd>
               </Command.Item>
             </Command.Group>
